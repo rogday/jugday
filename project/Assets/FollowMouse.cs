c47877416ed1303c31c11db058806a9fadf5d812
rogday <rogday@test.com>
2019-03-28 09:04:03 +0300

    test

---
+++ project/Assets/FollowMouse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    public float distance = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = distance;
+        transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
+    }
+}
