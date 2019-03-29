fd47e34e45e9671c7e563f58833efeceed4e0a49
rogday <rogday@test.com>
2019-03-29 08:10:19 +0300

    2d gen

---
+++ project/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+
+    public bool autoUpdate;
+
+    public void GenerateMap() {
+        var noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+
+        var display = FindObjectOfType<MapDisplay>();
+        display.DrawNoiseMap(noiseMap);
+    }
+}
---
+++ project/Assets/Scripts/Noise.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Noise
+{
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale) {
+        var noiseMap = new float[mapWidth, mapHeight];
+
+        scale = Mathf.Max(scale, 0.0001f);
+
+        for (int y = 0; y < mapHeight; ++y) {
+            for(int x = 0; x < mapWidth; ++x) {
+                float sampleX = x/scale;
+                float sampleY = y/scale;
+
+                float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
+                noiseMap[x, y] = perlinValue;
+            }
+        }
+
+        return noiseMap;
+    }
+}
