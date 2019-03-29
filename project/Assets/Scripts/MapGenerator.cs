4f3a6cd87f2bba6fe3e3c07bab8a55a35fbf48ab
rogday <rogday@test.com>
2019-03-29 09:48:10 +0300

    octaves

--- project/Assets/Scripts/MapGenerator.cs
+++ project/Assets/Scripts/MapGenerator.cs
@@ -8,12 +8,30 @@ public class MapGenerator : MonoBehaviour
     public int mapHeight;
     public float noiseScale;
 
+    public int octaves;
+    [Range(0,1)]
+    public float persistance;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
     public bool autoUpdate;
 
     public void GenerateMap() {
-        var noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        var noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale,
+            octaves, persistance, lacunarity, offset);
 
         var display = FindObjectOfType<MapDisplay>();
         display.DrawNoiseMap(noiseMap);
+    }
+
+    private void OnValidate() {
+        mapWidth = Mathf.Max(mapWidth, 1);
+        mapHeight = Mathf.Max(mapHeight, 1);
+        noiseScale = Mathf.Max(noiseScale, float.Epsilon);
+
+        octaves = Mathf.Max(octaves, 1);
+        lacunarity = Mathf.Max(lacunarity, 1);
     }
 }
